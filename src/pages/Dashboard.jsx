c0e56e73476cdfe1127504825c1f56b4e0281fca
khuloodmaqbool <khuloodmaqbool3@gmail.com>
2025-04-27 18:04:05 +0500

    ready production build

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,7 +3,7 @@ import TaskBoard from "../components/Taskboard";
 export default function Dashboard() {
   return (
     <div>
-      <h1>Task Tracker Dashboard</h1>
+      <h1>Task Manager Dashboard</h1>
       <TaskBoard />
     </div>
   );
